b691129dbbb69ebd2282d1246ac3fc6b74037d07
Leonardo Santos <leonardo.l.santos@gmail.com>
2024-04-06 14:50:30 -0300

    adding head

--- lista-03.01.py
+++ lista-03.01.py
@@ -1,10 +1,12 @@
 # lista 03 - questão 01
+# Faça um algoritmo que leia a primeira letra do estado civil de uma pessoa e mostre uma
+# mensagem com a sua descrição (Solteiro, Casado, Viúvo, Divorciado). Mostre uma
+# mensagem de erro, se necessário.
 resposta_usuario = ''
 
 while True:
    print('Entre com primeira letra estado civil.')
    resposta_usuario = input('ou "sair" para encerrar: ').upper()
-   print(resposta_usuario)
    if resposta_usuario == 'C':
       print("Casado")
    elif resposta_usuario == 'S':
@@ -17,5 +19,4 @@ while True:
       break
    else:
       print("nao aceito")
-
 
