5cc66c61d80a5037a2ec2e14bf683af8e5b944af
Leonardo Santos <leonardo.l.santos@gmail.com>
2024-04-07 09:37:06 -0300

    Questao 4 - lista 3

---
+++ lista-03-04.py
@@ -0,0 +1,33 @@
+''' 
+ lista 03 - questão 04
+ Em uma cidade, a prefeitura deseja classificar os motoristas com base em suas infrações
+ de trânsito no último ano. O objetivo é criar um programa de reeducação para os
+ motoristas que cometem infrações. Para isso, eles definiram as seguintes categorias:
+  a) Motorista Exemplar: Não cometeu nenhuma infração.
+  b) Motorista Atento: Cometeu até 3 infrações.
+  c) Motorista Desatento: Cometeu entre 4 e 6 infrações.
+  d) Motorista Perigoso: Cometeu mais de 6 infrações.
+ Escreva um programa que peça ao usuário para inserir o número de infrações que
+ cometeu no último ano e informe em qual categoria ele se encaixa.
+ Exemplo Entrada Exemplo Saída
+   5 Categoria: Motorista Desatento
+ '''
+numero_infracao = 0
+while True:
+   print('Entre com número de infrações que cometeu no último ano')
+   numero_infracao = int(input('ou "-1" para encerrar: '))
+   if numero_infracao == -1:
+      break
+   elif type(numero_infracao)!= int:
+      print("Número de infrações que cometeu no último ano deve ser um valor inteiro!")
+      continue
+
+   if numero_infracao == 0:
+      print('Categoria: Motorista Exemplar')
+   elif numero_infracao <= 3:
+      print('Categoria: Motorista Atento')
+   elif numero_infracao <= 6:
+      print('Categoria: Motorista Desatento')
+   elif numero_infracao > 6:
+      print('Categoria: Motorista Perigoso')
+  
