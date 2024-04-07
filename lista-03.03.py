43a8b8b900dcf17c6aa46ee0769baff5fbf0801d
Leonardo Santos <leonardo.l.santos@gmail.com>
2024-04-07 09:14:02 -0300

    Questao 3 - lista 3

---
+++ lista-03.03.py
@@ -0,0 +1,18 @@
+''' 
+ lista 03 - questão 03
+ Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-
+ Domingo, 2- Segunda, etc.). Exibir mensagem “Valor Inválido” caso um número
+ inesperado for informado.
+ '''
+dia_da_semana = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo']
+numero_dia_da_semana = 0
+
+while True:
+   print('Entre com um número correspondente ao dia da semana.')
+   numero_dia_da_semana = int(input('ou "0" para encerrar: '))
+   if numero_dia_da_semana == 0:
+      break
+   elif numero_dia_da_semana < 1 or numero_dia_da_semana > 7:
+      print("Número correspondente ao dia da semana deve ser valor entre 1 e 7!")
+      continue
+   print(f'Dia da semana: {dia_da_semana[numero_dia_da_semana - 1]}')
